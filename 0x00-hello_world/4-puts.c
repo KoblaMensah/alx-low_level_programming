05adb143a7b7750d49ea0da2b861ca5b281651bd
KoblaMensah <kobla.mensahh@gmail.com>
2023-02-20 10:37:13 -0800

    added three extra files to remote

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (sucess)
+ *
+ * puts - prints to the standard output
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
+
