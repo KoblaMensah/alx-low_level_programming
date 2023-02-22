e1389c9b553e342eb706ac676d6bf4ac94b3d4d3
KoblaMensah <kobla.mensahh@gmail.com>
2023-02-22 06:13:17 -0800

    hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,13 @@ int main(void)
 int n;
 for (n = 48; n <= 57; n++)
 {
+	putchar(n);
 if (n != 57)
 {
-	putchar(n);
 	putchar(44);
 	putchar(32);
 }
-else
-	putchar(n);
 }
+putchar('\n');
 return (0);
 }
