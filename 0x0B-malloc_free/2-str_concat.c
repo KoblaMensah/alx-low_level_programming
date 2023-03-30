333c2d04d4b3ccb13d0e95773a56f3f3df6b6c41
KoblaMensah <kobla.mensahh@gmail.com>
2023-03-30 09:46:37 -0700

    fixed an output error in 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 int i, ii;
 char *str;
 
-if (s1 == NULL || s2 == NULL)
-	return (NULL);
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
 
 i = ii = 0;
 
